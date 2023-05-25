07e0a96fcf451d9a516862e3850e9bf098b28131
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-05-25 13:41:06 +0800

    update dictionary

--- parse-csv.py
+++ parse-csv.py
@@ -33,6 +33,6 @@ class WordsJSONGenerator(object):
             json.dump(self.words, f)
 
 if __name__ == "__main__":
-    wjg = WordsJSONGenerator(file_name='Words200.csv')
+    wjg = WordsJSONGenerator(file_name='Words300.csv')
     wjg.read_csv_list()
     wjg.save_words2json()
